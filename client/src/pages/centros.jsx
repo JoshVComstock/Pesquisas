aff75c480de707a7fd6bfd4f0667648820756123
JoshVComstock <josedanielveizaga@gmail.com>
2023-01-13 19:49:02 -0400

    version 0.2

---
+++ client/src/pages/centros.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const centros = () => {
+  return (
+    <div>centros</div>
+  )
+}
+
+export default centros
---
+++ serve/app/Http/Controllers/CentrosController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Centros;
+use Illuminate\Http\Request;
+
+class CentrosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Centros::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $centros= new Centros();
+        $centros->nombre=$request->nombre;
+        $centros->id_redes=$request->id_redes;
+        $centros->telefono=$request->telefono;
+        $centros->id_ciudades=$request->ciudades;
+        $centros->area=$request->area;
+        $centros->seguimiento_casos=$request->seguimiento_casos;
+        $centros->contacto=$request->contacto;
+        $centros->save();
+        
+        return $centros;
+    }
+    public function update(Request $request, $id)
+    {
+        $centros= Centros::find($id);
+        $centros->nombre=$request->nombre;
+        $centros->id_redes=$request->id_redes;
+        $centros->telefono=$request->telefono;
+        $centros->id_ciudades=$request->ciudades;
+        $centros->area=$request->area;
+        $centros->seguimiento_casos=$request->seguimiento_casos;
+        $centros->contacto=$request->contacto;
+        $centros->save();
+        return $centros;
+    }
+    public function destroy($id)
+    {
+        return Centros::destroy($id);
+    }
+}
