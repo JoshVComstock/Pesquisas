9b263c7a7c83dbf7299db7dd596b3681570c36da
JoshVComstock <josedanielveizaga@gmail.com>
2023-01-13 20:07:33 -0400

    version 0.3

--- client/src/pages/centros.jsx
+++ client/src/pages/centros.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const centros = () => {
+const Centros = () => {
   return (
-    <div>centros</div>
+    <Container>
+      <Titulo>Centros</Titulo>
+      <div>
+        <table>
+            <p>Hola</p>
+            <p>que tal </p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </table>
+      </div>
+    </Container>
   )
 }
 
-export default centros
+export default Centros
+const Container=styled.div`
+  
+`;
+const Titulo=styled.h1`
+    display: flex;
+    background: blueviolet;
+    justify-content: center;
+`;
