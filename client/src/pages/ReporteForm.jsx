05b21ed0a0aab71b5c3de4080f424986f0132a51
TANHEHANZ <hantach10@gmail.com>
2023-05-12 00:30:17 -0400

    vista repordinamico

--- client/src/pages/ReporteForm.jsx
+++ client/src/pages/ReporteForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import styled from "styled-components";
 const ReporteForm = () => {
   const [campos, setCampos] = useState([]);
   const [filtros, setFiltros] = useState({});
@@ -14,41 +14,41 @@ const ReporteForm = () => {
     }
   };
 
-//   const handleFiltroChange = (event) => {
-//     const filtro = event.target.value;
-//     const campo = event.target.getAttribute('name');
+  //   const handleFiltroChange = (event) => {
+  //     const filtro = event.target.value;
+  //     const campo = event.target.getAttribute('name');
 
-//     setFiltros((prevFiltros) => ({
-//       ...prevFiltros,
-//       [campo]: filtro,
-//     }));
-//   };
-// const handleFiltroChange = (event) => {
-//     const filtro = event.target.value;
-//     const campo = event.target.getAttribute('name');
-  
-//     const [campoPrincipal, subCampo] = campo.split('.');
-  
-//     setFiltros((prevFiltros) => ({
-//       ...prevFiltros,
-//       [campoPrincipal]: {
-//         ...prevFiltros[campoPrincipal],
-//         [subCampo]: filtro,
-//       },
-//     }));
-//   };
-const handleFiltroChange = (event) => {
+  //     setFiltros((prevFiltros) => ({
+  //       ...prevFiltros,
+  //       [campo]: filtro,
+  //     }));
+  //   };
+  // const handleFiltroChange = (event) => {
+  //     const filtro = event.target.value;
+  //     const campo = event.target.getAttribute('name');
+
+  //     const [campoPrincipal, subCampo] = campo.split('.');
+
+  //     setFiltros((prevFiltros) => ({
+  //       ...prevFiltros,
+  //       [campoPrincipal]: {
+  //         ...prevFiltros[campoPrincipal],
+  //         [subCampo]: filtro,
+  //       },
+  //     }));
+  //   };
+  const handleFiltroChange = (event) => {
     const filtro = event.target.value;
-    const campo = event.target.getAttribute('name');
-  
-    if (campo === 'start' || campo === 'end') {
+    const campo = event.target.getAttribute("name");
+
+    if (campo === "start" || campo === "end") {
       const fechaNacimiento = {
-        start: filtros.fecha_nacimiento?.start || '',
-        end: filtros.fecha_nacimiento?.end || '',
+        start: filtros.fecha_nacimiento?.start || "",
+        end: filtros.fecha_nacimiento?.end || "",
       };
-  
+
       fechaNacimiento[campo] = filtro;
-  
+
       setFiltros((prevFiltros) => ({
         ...prevFiltros,
         fecha_nacimiento: fechaNacimiento,
@@ -60,7 +60,7 @@ const handleFiltroChange = (event) => {
       }));
     }
   };
-  
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -83,8 +83,10 @@ const handleFiltroChange = (event) => {
   };
 
   return (
-    <>
+    <Report>
       <form onSubmit={handleSubmit}>
+      <h1>Reporte Dinamico Pacientes</h1>
+
         <label>
           <input
             type="checkbox"
@@ -120,39 +122,114 @@ const handleFiltroChange = (event) => {
             <option value="f">Femenino</option>
           </select>
         )}
-       <label>
-    <input
-      type="checkbox"
-      value="fecha_nacimiento"
-      onChange={handleCheckboxChange}
-    />
-    Fecha de Nacimiento
-  </label>
-  {campos.includes("fecha_nacimiento") && (
-    <div>
-      <label>Desde:</label>
-      <input type="date" name="start" onChange={handleFiltroChange} />
-      <label>Hasta:</label>
-      <input type="date" name="end" onChange={handleFiltroChange} />
-    </div>
-  )}
+        <label>
+          <input
+            type="checkbox"
+            value="fecha_nacimiento"
+            onChange={handleCheckboxChange}
+          />
+          Fecha de Nacimiento
+        </label>
+        {campos.includes("fecha_nacimiento") && (
+          <article>
+            <label>Desde:</label>
+            <input type="date" name="start" onChange={handleFiltroChange} />
+            <label>Hasta:</label>
+            <input type="date" name="end" onChange={handleFiltroChange} />
+          </article>
+        )}
         <button type="submit">Generar Reporte</button>
       </form>
       {pdfUrl && (
         <div>
           <iframe
             src={pdfUrl}
-            width="100%"
+            width="50vw"
             height="500px"
             title="Vista previa del PDF"
           />
-          <a href={pdfUrl} download="reporte.pdf">
-            Descargar PDF
-          </a>
+        
         </div>
       )}
-    </>
+    </Report>
   );
 };
 
 export default ReporteForm;
+export const Report = styled.div`
+  width: 95%;
+  height: 100%;
+  background-color: transparent;
+  margin: 1em auto;
+  display: flex;
+  flex-direction: row;
+
+  & form {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 34%;
+    align-items: center;
+    margin: auto;
+    backdrop-filter: blur(7px) saturate(79%);
+    -webkit-backdrop-filter: blur(7px) saturate(79%);
+    background-color: #6f67d7;
+    border-radius: 12px;
+    border: 1px solid #0002;
+    margin: 1em;
+    gap: 1em;
+    color: #fff;
+    padding: 2%;
+    
+    & h1{
+        width:100%;
+        height:1.5em;
+        font-size:1em ;
+        text-transform:uppercase;
+
+    }
+    & button {
+      width: 100%;
+      padding: 0.6em 0;
+      cursor: pointer;
+      transition:all 0.5s ease;
+      border-radius:5px;
+      &:hover{
+        background-color:#6f67d7;
+        color:#fff;
+        text-transform:uppercase;
+        border:solid 1px #fff5;
+      }
+    }
+    &::before {
+      content: "";
+      bottom: -0.8em;
+      position: absolute;
+      border: solid 1px #0002;
+      width: 90%;
+      height: 0.69em;
+      background-color: #6e67d7ab;
+      z-index: 1;
+      border-radius: 0 0 5px 5px;
+    }
+    transition:all 0.5s ease-in-out;
+
+    &:hover{
+        width:34%;
+        &::before {
+        opacity:0;
+        }
+    }
+& article{
+    width:100%;
+    font-size:0.8em;
+}
+  }
+& div{
+    margin: 1em;
+    & iframe {
+    width: 50vw;
+    height:20em;
+  }
+}
+`;
--- client/src/pages/Reportesdinamicos.jsx
+++ client/src/pages/Reportesdinamicos.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import ReporteForm from "./ReporteForm";
-
+import styled from 'styled-components';
 const Reportesdinamicos = () => {
   return (
-    <div>Reportesdinamicos
-
-
+    <Div>
         <section>
-      <ReporteForm/>
-
+        <ReporteForm/>
         </section>
-    </div>
+        <section>
+        <ReporteForm/>
+        </section>
+    </Div>
   )
 }
 
 export default Reportesdinamicos
+export const Div = styled.section`
+  width: 100%;
+  height: 95%;
+  display:flex;
+  flex-wrap:wrap;
+  flex-direction:row;
+  gap:1em;
+  & section{
+height:100%;
+max-height:20em;
+min-height:15em;
+  }
+`;
